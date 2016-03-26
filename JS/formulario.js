07df4d833aad618bbd8fc71f9b23c696a2432a7f
AxlRUI <axelpforestrubio@hotmail.com>
2016-03-26 14:20:57 -0600

    dasd
    asd

---
+++ JS/formulario.js
@@ -0,0 +1,61 @@
+
+(function(){
+
+var formulario = document.formulario_login,
+	elementos = formulario.elements;
+
+
+var validarInputs = function(){
+	for (var i = 0; i < elementos.length; i++) {
+
+		if (elementos[i].type == "text" || elementos[i].type == "password") {
+
+			if (elementos[i].value.length == 0) {
+				console.log('El campo ' + elementos[i].name + ' esta incompleto');
+				elementos[i].className = elementos[i].className + " error";
+				return false;
+			} else {
+				elementos[i].className = elementos[i].className.replace(" error", "");
+			}
+		}
+	}
+
+
+	return true;
+};
+
+
+
+var enviar = function(e){
+	if (!validarInputs()) {
+		console.log('Falto validar los Input');
+		e.preventDefault();
+	} else {
+		console.log('Envia');
+		e.preventDefault();
+	}
+};
+
+var focusInput = function(){
+	this.parentElement.children[1].className = "label active";
+	this.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
+};
+
+var blurInput = function(){
+	if (this.value <= 0) {
+		this.parentElement.children[1].className = "label";
+		this.parentElement.children[0].className = this.parentElement.children[0].className + " error";
+	}
+};
+
+
+formulario.addEventListener("submit", enviar);
+
+for (var i = 0; i < elementos.length; i++) {
+	if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
+		elementos[i].addEventListener("focus", focusInput);
+		elementos[i].addEventListener("blur", blurInput);
+	}
+}
+
+}())
