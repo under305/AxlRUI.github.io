70a5a763369185912a9a83523c99a76427948d91
AxlRUI <axelpforestrubio@hotmail.com>
2016-04-08 08:09:58 -0500

    Update formulario.js

--- JS/formulario.js
+++ JS/formulario.js
@@ -108,7 +108,16 @@ function historia2(){
 	document.getElementById("fecha").innerHTML = "<div class='inventario1'>" +
 							+"<label class='label'>Inventario, negro existencias rojo agotado</label>"
 							+"<ul class='list'>"
-								+"<li class='in'>NOMBRE DEL EMPLEADO SOLICITUD ETC ETC</li>"
+								+"<li class='in'>empleado: juan manuel perez cebo <br> solictud: computadora <br>mac/codigo:6b:65b:65g:</li>"
+								+"<li class='in'>empleado: samuel auces martinez <br>solicitud:teclado <br>mac/codigo:658466a6856</li>"
+								+"<li class='in'>empleado: axel uriel rubio <br>solicitud: raton<br> mac/codigo:646dfs319887</li>"
+								+"<li class='in'>empleado: andrea barron mercado <br>solicitud: monitor mac/codigo:968546a351f8</li>"
+								+"<li class='in'>empleado: lorayne cristal sanchez <br>solicitud: computadora<br> mac/codigo:54:4h:8h:5j:</li>"
+								+"<li class='in'>empleado: francisco valerio sandoval <br>solicitud: teclado <br>mac/codigo:6846s6d46f</li>"
+								+"<li class='in'>luis alejandro martinez ramirez <br>solicitud: monitor<br> mac/codigo: 6489af984g6</li>"
+								+"<li class='in'>eduardo casique gonzalez <br>solicitud: teclado<br> mac/codigo:541a6sf4a6f49</li>"
+								+"<li class='in'>david ferra gomez<br> solicitud: computadora <br>mac/codigo:5g:8f:9r:2S:8s:</li>"
+								+"<li class='in'>martha dolores garcia <br>solicitud: monitor <br>mac/codigo:15686s84fa</li>"
 							+"</ul>"
-						+"</div>";
+							+"</div>";
 };
