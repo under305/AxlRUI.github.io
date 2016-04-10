4c6a4d88dbcc68e484ffd629044e029d09f14024
AxlRUI <axelpforestrubio@hotmail.com>
2016-04-10 18:43:10 -0500

    6:42 pm

--- medic/js/scripts.js
+++ medic/js/scripts.js
@@ -20,3 +20,24 @@ function main(){
 			$(this).children('.hijo').slideToggle();
 		});
 }
+$(function(){
+  $(".slide").slidesjs({
+    play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "fade",
+        // [string] Can be either "slide" or "fade".
+      interval: 3000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: true,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: true,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+  });
+});
